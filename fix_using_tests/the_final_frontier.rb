201834b02097ad1ac9d1135645be4daf6ee0b4a9
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-25 14:06:15 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -8,15 +10,19 @@ def state_log(star_date)
 end
 
 def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(crew)
+  crew.map do |mem|
+    "Hello #{mem}."
+  #binding.pry
+end
 end
 
 def engage
-  date = generate_star_date
-  state_log(date)
+  #date = generate_star_date
+  #state_log(date)
   greet_crew(crew)
+  #binding.pry
 end
